b86a6f626798c55738aadd97d01f9d6bcde1423c
josepablomartinez <toche.cr@gmail.com>
2020-07-15 17:28:16 -0500

    Add Dashboard component

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,8 +27,7 @@ module.exports = {
         },
     },
     rules: {
-        // You can do your customizations here...
-        // For example, if you don't want to use the prop-types package,
-        // you can turn off that recommended rule with: 'react/prop-types': ['off']
+        'react/prop-types': ['off'],
+        'react/no-unescaped-entities': ['error', { forbid: ['>', '}'] }],
     },
 };
--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Dashboard from './Dashboard'
 
-export default function App() {
-    const [count, setCount] = useState(0);
-    return (
-        <div>
-            This is a sample stateful and server-side
-            rendered React application.
-            <br />
-            <br />
-      Here is a button that will track
-      how many times you click it:
-            <br />
-            <br />
-            <button onClick={() => setCount(count + 1)}>{count}</button>
-        </div>
-    );
+// export default function App() {
+//     const [count, setCount] = useState(0);
+//     return (
+//         <div>
+//             Mi puntaje del dia:
+//             <br />
+//             <button onClick={() => setCount(count + 1)}>{count}</button>
+//         </div>
+//     );
+// }
+
+// class App extends App.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             date: new Date().getDate(),
+//             month: new Date().getMonth()
+//         }
+//     }
+// }
+
+
+const Fitness = () => {
+    return <Dashboard date={new Date().getDate()} month={new Date().getMonth()} />
 }
+
+
+export default Fitness
